bb4a6629e3480c9c8188071cf026c6aab1a2c04b
Haoran Feng <harrifeng@gmail.com>
2017-11-24 12:10:54 +0000

    typo

--- git/link.sh
+++ git/link.sh
@@ -1,4 +1,4 @@
 rm -f ~/.gitignore_global
 ln -s ~/github/dotfiles/git/gitignore_global ~/.gitignore_global
 rm -f ~/.gitconfig
-ln -s ~/github/dotfiles/git/gitignore ~/.gitconfig
+ln -s ~/github/dotfiles/git/gitconfig ~/.gitconfig
