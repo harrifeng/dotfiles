1a14bd4ecfe5f4e75fc7e56e45ee730fc8835bf3
Haoran Feng <harrifeng@gmail.com>
2018-02-27 14:11:52 +0800

    new setting

--- run.sh
+++ run.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 ln -sf ~/github/dotfiles/bash/dot_bashrc ~/.bashrc
 ln -sf ~/github/dotfiles/tmux/dot_tmux.conf ~/.tmux.conf
+ln -sf ~/github/dotfiles/git/dot_gitconfig ~/.gitconfig
+ln -sf ~/github/dotfiles/git/dot_gitignore_global ~/.gitignore_global
