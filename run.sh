6969b4de3abda114d4ba408de82ef381e26b350d
Haoran Feng <harrifeng@gmail.com>
2018-05-16 06:41:31 +0000

    ok by now

--- run.sh
+++ run.sh
@@ -3,3 +3,4 @@ ln -sf ~/github/dotfiles/bash/dot_bashrc ~/.bashrc
 ln -sf ~/github/dotfiles/tmux/dot_tmux.conf ~/.tmux.conf
 ln -sf ~/github/dotfiles/git/dot_gitconfig ~/.gitconfig
 ln -sf ~/github/dotfiles/git/dot_gitignore_global ~/.gitignore_global
+ln -sf ~/github/dotfiles/ssh/config ~/.ssh/config
