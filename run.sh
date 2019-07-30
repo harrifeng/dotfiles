3f284625e9b2e3f42b5db9e6f89179904e75a94e
Haoran Feng <harrifeng@gmail.com>
2019-07-30 06:44:29 +0000

    util is better name

--- run.sh
+++ run.sh
@@ -4,3 +4,4 @@ ln -sf ${CURRENT_PATH}/bash/dot_bashrc ~/.bashrc
 ln -sf ${CURRENT_PATH}/tmux/dot_tmux.conf ~/.tmux.conf
 ln -sf ${CURRENT_PATH}/git/dot_gitconfig ~/.gitconfig
 ln -sf ${CURRENT_PATH}/git/dot_gitignore_global ~/.gitignore_global
+ln -sf ${CURRENT_PATH}/util ~/util
