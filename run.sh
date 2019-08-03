dea3a82d84ad9b97f268b4de0934aa17b5d8ab59
vagrant <vagrant@debian-workspace>
2019-08-03 02:31:14 +0000

    we use oh-my-zsh instead

--- run.sh
+++ run.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 CURRENT_PATH=$(cd `dirname $0` && pwd)
-ln -sf ${CURRENT_PATH}/bash/dot_bashrc ~/.bashrc
 ln -sf ${CURRENT_PATH}/tmux/dot_tmux.conf ~/.tmux.conf
 ln -sf ${CURRENT_PATH}/git/dot_gitconfig ~/.gitconfig
 ln -sf ${CURRENT_PATH}/git/dot_gitignore_global ~/.gitignore_global
