07efe8dca1971bf330a0a4f7d7629ea953a46bbf
Haoran Feng <harrifeng@gmail.com>
2019-08-03 02:45:51 +0000

    use oh-my-zsh

--- run.sh
+++ run.sh
@@ -3,3 +3,4 @@ CURRENT_PATH=$(cd `dirname $0` && pwd)
 ln -sf ${CURRENT_PATH}/tmux/dot_tmux.conf ~/.tmux.conf
 ln -sf ${CURRENT_PATH}/git/dot_gitconfig ~/.gitconfig
 ln -sf ${CURRENT_PATH}/git/dot_gitignore_global ~/.gitignore_global
+sh -c "$(wget -O- https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
